da437a6d261e5547a3659a062ce9535a7fc2b165
jeonha01 <kangjeonha@gmail.com>
2024-05-24 23:03:26 +0900

    third commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const logo = document.querySelector("#logo") // 로고 버튼
+
+logo.addEventListener("click", lobby) // 로고 버튼 누르면 메인 페이지로 이동함
+
+function lobby() { // 메인페이지로 이동하는 함수
+    window.location.href = 'file:///C:/Users/hi/OneDrive/%EB%B0%94%ED%83%95%20%ED%99%94%EB%A9%B4/%EC%86%8C%EC%9B%A8%EA%B3%B5%20%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8/index.html'
+}
+
+const plusButton = document.querySelector("#plus")
+const minusButton = document.querySelector("#minus")
